bbdec0e57047295b46cdddb151d6eedf69f1ab38
Fetyali7 <fetyali7@gmail.com>
2023-02-17 09:56:19 -0500

    Better CSS added

--- 341-project/src/Component/Navbar.js
+++ 341-project/src/Component/Navbar.js
@@ -8,14 +8,16 @@ export const Navbar = ({changeTab}) => {
     //     <div className='navItem' onClick={()=> {changeTab("Contact")}}>Contact</div>
     //     <div className='navItem' onClick={()=> {changeTab("Help")}}>Help</div>        
     // </div>
-    <div class="topnav">
-    <a class="active" onClick={()=> {changeTab("Home")}}>JobHunt</a>
-    <a  onClick={()=> {changeTab("Finding")}}>FindJob</a>
-    <a  onClick={()=> {changeTab("Posting")}}>PostJob</a>
-    <a onClick={()=> {changeTab("Search")}}>Search</a>
-    <div class="topnav-right">
-      <a  onClick={()=> {changeTab("Signup")}}>SignUp</a>
-      <a onClick={()=> {changeTab("Aboutcompany")}}>About</a>
+    <div className='Navbar__Background'>
+      <div class="topnav">
+      <a class="active" onClick={()=> {changeTab("Home")}}>JobHunt</a>
+      <a  onClick={()=> {changeTab("Finding")}}>FindJob</a>
+      <a  onClick={()=> {changeTab("Posting")}}>PostJob</a>
+      <a onClick={()=> {changeTab("Search")}}>Search</a>
+      <div class="topnav-right">
+        <a  onClick={()=> {changeTab("Signup")}}>SignUp</a>
+        <a onClick={()=> {changeTab("Aboutcompany")}}>About</a>
+      </div>
     </div>
   </div>
   )
