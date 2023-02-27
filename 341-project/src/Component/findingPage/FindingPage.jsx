cdf6dabb23473522940e466691729f6f12151d52
mycoal2 <michaelhong136@gmail.com>
2023-02-27 16:19:57 -0500

    User Story #5

--- 341-project/src/Component/findingPage/FindingPage.jsx
+++ 341-project/src/Component/findingPage/FindingPage.jsx
@@ -3,21 +3,73 @@ import Axios from 'axios';
 import { FindingPage } from '..'
 
 export const Findingpage= ({Findingpage}) => {
+  const [sortCompany, setSortCompany] = useState("");
+  const [sortPosition, setSortPosition] = useState("");
+  const [search, setSearch] = useState("");
 
   const [jobList, setJobList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+
     useEffect(() => {
         Axios.get("http://localhost:3001/JobPostings").then((response) => {
             setJobList(response.data);
+            setFilteredList(response.data);
         });
     }, []);
 
+  const handleSearch = (e) => {
+    // if(e.target.value === "") {
+    //   setFilteredList(jobList);
+    // }
+    setSearch(e.target.value);
+    console.log(search);
+  }
+  const handleSortCompany = () => {
+    setSortPosition("")
+    //setSortCompany(search);
+    handleFilterName();
+  }
+  const handleSortPosition = () => {
+    setSortCompany("");
+    //setSortPosition(search);
+    handleFilterPosition();
+  }
+
+  const handleFilterName = () => {
+    let filtered = jobList.filter(job => {
+      return job.companyName.toLowerCase().includes(search.toLowerCase());
+    })
+  console.log(filtered);
+  setFilteredList(filtered);
+  }
+
+  const handleFilterPosition = () => {
+    let filtered = jobList.filter(job => {
+      return job.jobDescription.toLowerCase().includes(search.toLowerCase());
+    })
+  console.log(filtered);
+  setFilteredList(filtered);
+  }
+
   return (
-    <div>This is where you can find a job!
-      {/* {jobList.map((value) => 
-      <div>
-        {value.companyName}
-      </div>
-        )} */}
+    <div>
+      <input onChange={handleSearch}></input>
+      <button onClick={handleSortCompany}>Search Company</button>
+      <button onClick={handleSortPosition}>Search Position</button>
+      <button onClick={() => setFilteredList(jobList)}> X </button>
+      {jobList.length > 0 ? (
+        <div> {filteredList.reverse().map((value) => 
+          <div>
+            <div>{value.companyName}</div>
+            <div>{value.phoneNumber}</div>
+            <div>{value.employerName}</div>
+            <div>{value.jobDescription}</div>
+            <div>{value.location}</div>
+          </div>
+        )}
+        </div>
+      ) : (<div>This is where you can find a job!</div>)
+      }
     </div>
   )
 }
