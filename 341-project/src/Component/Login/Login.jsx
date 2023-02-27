7d2c26569de1df0240261640684d8f5c9bb89e60
Kadestery <k_keati@live.concordia.ca>
2023-02-27 18:32:53 -0500

    Added Login

--- 341-project/src/Component/Login/Login.jsx
+++ 341-project/src/Component/Login/Login.jsx
@@ -1,10 +1,23 @@
+import React, {useState} from "react";
 export const Login = () => {
-    return(
-        <form>
-            <label for="email">email</label>
-            <input type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
-            <label for="password">password</label>
-            <input type="password" placeholder="*********" id="password" name="password"/>
-        </form>
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email)
+    }
+     return(
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">email</label>
+                <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
+                <label htmlFor="password">password</label>
+                <input value={pass} type="password" placeholder="*********" id="password" name="password"/>
+                <button type="submit">Log In</button>
+            </form>
+            <button>Don't have an account? Register here.</button>
+        </div>
     )
 }
