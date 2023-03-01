e59b982a0a7db325126f72579174c06782f19911
Rihazul <rihazulislam@gmail.com>
2023-03-01 14:14:42 -0500

    Added the description

--- 341-project/src/Component/Aboutpage/Aboutpage.jsx
+++ 341-project/src/Component/Aboutpage/Aboutpage.jsx
@@ -2,7 +2,11 @@ import React, { useState } from 'react'
 import './Aboutpage.css'
 export const Aboutpage = () =>{
   return (
-    <h1>Rihazul, write the description for aboutpage here</h1>
+    <><h1>About JobHunt</h1>
+    <> </> 
+    <p>JobHunt makes life easier to apply and to reasearch the desired job. JobHunt facilitates employers to post amazing jobs 
+      for applicants as many as they want. JobHunt's mission is to help people find jobs and companies they love.</p></>
+    
   )
 }
 
