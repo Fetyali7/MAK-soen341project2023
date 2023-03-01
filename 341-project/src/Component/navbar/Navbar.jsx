089c4863178ea387c082ede4f3bc7e4d938bebeb
yep-pushian <102338675+yep-pushian@users.noreply.github.com>
2023-03-01 17:41:51 -0500

    implemented logo

--- 341-project/src/Component/navbar/Navbar.jsx
+++ 341-project/src/Component/navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import logo from '../../images/JobHunt_Logo.JPG';
 import './navbar.css'
 
-export const Navbar = ({changeTab}) => {
+export const Navbar = ({ changeTab }) => {
   return (
     // <div className='navbar'>
     //     <h1 className='navTitle' onClick={()=> {changeTab("Home")}}>JobHunt</h1>
@@ -10,18 +11,22 @@ export const Navbar = ({changeTab}) => {
     //     <div className='navItem' onClick={()=> {changeTab("Help")}}>Help</div>        
     // </div>
     <div className='Navbar__Background'>
+
+      <img src={logo} alt="JobHunt LOGO"/>
       <div class="topnav ">
-      <a href="#Home" class="active" onClick={()=> {changeTab("Home")}}>JobHunt</a>
-      <a href="#Finding" className='' onClick={()=> {changeTab("Finding")}}>FindJob</a>
-      <a href="#PostJob" onClick={()=> {changeTab("Posting")}}>PostJob</a>
-      {/* <a href="#Search" onClick={()=> {changeTab("Search")}}>Search</a> */}
-      <div class="topnav-right">
-        <a  onClick={()=> {changeTab("Login")}}>Login</a>
-        <a  onClick={()=> {changeTab("Signup")}}>SignUp</a>
-        <a onClick={()=> {changeTab("Aboutpage")}}>About</a>
+        <a href="#Home" class="active" onClick={() => { changeTab("Home") }}>JobHunt</a>
+        <a href="#Finding" className='' onClick={() => { changeTab("Finding") }}>FindJob</a>
+        <a href="#PostJob" onClick={() => { changeTab("Posting") }}>PostJob</a>
+        <a href="#Search" onClick={() => { changeTab("Search") }}>Search</a>
+
+        <div class="topnav-right">
+          <a onClick={() => { changeTab("Login") }}>Login</a>
+          <a onClick={() => { changeTab("Signup") }}>SignUp</a>
+          <a onClick={() => { changeTab("Aboutcompany") }}>About</a>
+        </div>
       </div>
     </div>
-  </div>
+
   )
 }
 
