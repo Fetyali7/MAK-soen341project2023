9b21f409a93c755fb534ac17f8fe8f2faf44a3ef
Daniel Wegrzyn <danny.wegrzyn@hotmail.com>
2023-03-01 17:22:10 -0500

    added about page css

--- 341-project/src/Component/Aboutpage/Aboutpage.jsx
+++ 341-project/src/Component/Aboutpage/Aboutpage.jsx
@@ -2,11 +2,15 @@ import React, { useState } from 'react'
 import './Aboutpage.css'
 export const Aboutpage = () =>{
   return (
-    <><h1>About JobHunt</h1>
-    <> </> 
-    <p>JobHunt makes life easier to apply and to reasearch the desired job. JobHunt facilitates employers to post amazing jobs 
-      for applicants as many as they want. JobHunt's mission is to help people find jobs and companies they love.</p></>
-    
+    <div className='about section__padding gradient__bg'>
+      <div className='about-heading'>
+        <h1>About JobHunt</h1>
+      </div>
+      <div className='about-content'> 
+        <p>JobHunt makes life easier to apply and to reasearch the desired job. JobHunt facilitates employers to post amazing jobs 
+        for applicants as many as they want. JobHunt's mission is to help people find jobs and companies they love.</p>
+      </div>
+    </div>
   )
 }
 
