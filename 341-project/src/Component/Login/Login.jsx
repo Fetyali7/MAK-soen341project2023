f1af7ff4ce75277242b2ac9190859750c458df2b
Fetyali7 <fetyali7@gmail.com>
2023-03-20 12:38:03 -0400

    Login page is done! #42

--- 341-project/src/Component/Login/Login.jsx
+++ 341-project/src/Component/Login/Login.jsx
@@ -8,66 +8,54 @@ export const Login = ({ changeTab, setCurrentForm }) => {
     const [LoginList, setLoginList] = useState([]);
     const [filteredList, setFilteredList] = useState([]);
 
-    // useEffect(() => {
-    //     Axios.get("http://localhost:3001/userSu").then((response) => {
-    //         setLoginList(response.data);
-    //         setFilteredList(response.data);
-    //     });
-    // }, []);
+    useEffect(() => {
+        Axios.get("http://localhost:3001/userSu").then((response) => {
+            setLoginList(response.data);
+            setFilteredList(response.data);
+        });
+    }, []);
 
-    // const handleEmail = (e) => {
-    //     setEmail(e.target.value);
-    // }
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
 
-    // const handlePass = (e) => {
-    //     setPass(e.target.value); 
-    // }
+    const handlePass = (e) => {
+        setPass(e.target.value); 
+    }
 
       
     
-    //   const handleLogin = () => {
-    //     let filtered = LoginList.filter(user => {
-    //         return user.email.includes(email1);
-    //       })
-    //     let email = LoginList.find(user => user.email===email1).email;
-    //     let password = LoginList.find(user => user.email===email1).password;
-    //     let username = LoginList.find(user => user.email===email1).username;
-    //     if(email===email1 && password===pass1){
+      const handleLogin = () => {
+        let filtered = LoginList.filter(user => {
+            return user.email.includes(email1);
+          })
+        let email = LoginList.find(user => user.email===email1).email;
+        let password = LoginList.find(user => user.email===email1).password;
+        let username = LoginList.find(user => user.email===email1).username;
+        if(email===email1 && password===pass1){
         
-    //         setFilteredList(filtered);
-    //         alert("Welcome " + username + "!");
-    //         }
-    //     else{
-    //         alert("Please try again!");
-    //     }
+            setFilteredList(filtered);
+            alert("Welcome " + username + "!");
+            }
+        else{
+            alert("Please try again!");
+        }
         
-    //   }
+      }
      return(
         <div className="auth-form-container">
             <h2>Login</h2>
             <form className="login-form">
                 <div className='login-username'>
                     <label htmlFor="email">Email</label>
-                    <input value={email1} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                    <input value={email1} type="email" placeholder="youremail@gmail.com" id="email" name="email" onChange={handleEmail}/>
                 </div>
                 <div className='login-password'>
                     <label htmlFor="password">Password</label>
-                    <input value={pass1} type="password" placeholder="*********" id="password" name="password"/>
+                    <input value={pass1} type="password" placeholder="*********" id="password" name="password" onChange={handlePass}/>
                 </div>
-                    <button name="userBut">Log In</button>
+                    <button name="userBut" onClick={handleLogin}>Log In</button>
             </form>
-            {/* {LoginList.length > 0 ? (
-                <div> {filteredList.map((value) => 
-                    <div className='signupcard'>
-                    <div className='signupcard-username'>Username: {value.username}</div>
-                    <div className='signupcard-email'>email: {value.email}</div>
-                    <div className='signupcard-password'>password: {value.password}</div>
-                    <div className='signupcard-apliemp'>apliemp: {value.apliemp}</div>
-                </div>
-                )}
-                </div>
-                ) : (<div>This is where you can find a job!</div>)
-            } */}
         </div>
         
     )
